a2e597b494f00714dc75c2112547ee88f7248f59
AlexanderPaul1983 <alexanderpaul@gmx.de>
2024-06-18 10:40:43 +0200

    fixed issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './comStyle.css'
 import {useNavigate} from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+
 
 
 function Header() {
-  const dispatch = useDispatch();
+  
   const navigate = useNavigate();
   const handleClick = () => {
     navigate('/cart');
